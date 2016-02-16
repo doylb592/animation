0d4c8ec5b961263dc06c6f7c79945a769525d1e2
Bridget Doyle <bridgetdoyle99@gmail.com>
2016-02-16 17:35:47 -0500

    classwork

--- classwork/classwork.js
+++ classwork/classwork.js
@@ -1,21 +1,49 @@
 /*
   Classwork
 */
-var ballX = 0;
-var speed = 3;
+var circleX = 0;
+var circleY = 0;
+var speed1 = 5;
+var speed2 = 5;
+var circleColor = "blue";
 
 function setup(){
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth,windowHeight);
+
 }
 
 function draw(){
-  background("white");
-  ellipse(ballX, height/2, 50,50);
 
-  if (ballX >= width){
-    speed = -3;
-  }
+background("white");
 
-  ballX = ballX + speed;
+  fill("lavender");
+    if(mouseIsPressed){
+      fill(circleColor);
+
+}
+
+  ellipse(circleX,350,100,100);
+
+  circleX = circleX + speed1;
+
+    if(circleX >= width){
+      speed1 = -5;
+    }
+
+    if (circleX <= 0){
+      speed1 = 5;
+    }
+
+  ellipse(650,circleY,100,100);
+
+  circleY = circleY + speed2;
+
+    if(circleY >= height){
+      speed2 = -5;
+    }
+
+    if (circleY <= 0){
+      speed2 = 5;
+    }
 
 }

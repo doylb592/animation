391644518b4658bb94398f24c9630cecd2a37cd0
Bridget Doyle <bridgetdoyle99@gmail.com>
2016-04-19 17:07:52 -0400

    done
    done

--- homework/homework.js
+++ homework/homework.js
@@ -2,14 +2,71 @@
   Homework
 */
 
-//declare your variables
+//===================
+// variables
+//===================
+
+var hk;
+var dd;
+
+var movinghk = 0;
+var movingdd = 560;
+
+var speed1 = 5;
+var speed2 = 9.5;
+
+function preload(){
+  london = loadImage("images/london.png");
+  flowers = loadImage("images/flowers.jpg");
+  hk = loadImage("images/hello_kitty.png");
+  dd = loadImage("images/dear_daniel.png");
+}
 
 function setup(){
-  createCanvas(windowWidth,windowHeight);
+  createCanvas(london.width,london.height);
+  frameRate(30);
 
-  //set things up
 }
+
+//===================
+// background
+//===================
 
 function draw(){
-  //draw
-}
+  if (mouseIsPressed){
+    background(flowers,0,0,london.width,london.height);
+  }
+  else {
+    background(london,0,0,london.width,london.height);
+  }
+
+//===================
+// characters
+//===================
+
+
+   image(hk,movinghk,380);
+
+   movinghk = movinghk + speed1;
+
+      if(movinghk >= width - 190){
+        speed1 = -5;
+      }
+
+      if (movinghk <= 0){
+        speed1 = 5;
+      }
+
+    image(dd,movingdd, 315);
+
+    movingdd = movingdd + speed2;
+
+      if(movingdd >= width -250){
+        speed2 = -5;
+      }
+
+      if (movingdd < -45){
+        speed2= 5;
+      }
+
+  }
